8412e37c6448ab68bfc00fd121434b355fdd14a9
DivyanshMangla <divyanshmangla70@gmail.com>
2025-04-12 18:53:20 +0530

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "../components/header";
+import { ClerkProvider } from '@clerk/nextjs'
+const inter = Inter({subsets : ["latin"]})
+
+export const metadata: Metadata = {
+  title: "Weath Manager",
+  description: "A baasic wealth manager to monitor your wealth",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${inter.className}`}
+      >
+        <Header/>
+        <main className="min-h-screen"> {children} </main>
+        <footer className="bg-blue-50 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+           <p>Made with Skills</p>
+          </div>
+        </footer>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
